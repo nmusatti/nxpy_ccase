0041e0a9a48161a13f4974301b858b6ac59c0fcf
Nicola Musatti <nicola.musatti@gmail.com>
2018-10-14 17:14:21 +0200

    Cleaned up comments

--- nxpy/ccase/__init__.py
+++ nxpy/ccase/__init__.py
@@ -1,11 +1,11 @@
 # nxpy.ccase package ---------------------------------------------------------
 
-# Copyright Nicola Musatti 2011 - 2012
+# Copyright Nicola Musatti 2011 - 2018
 # Use, modification, and distribution are subject to the Boost Software
 # License, Version 1.0. (See accompanying file LICENSE.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-# See http://nxpy.sourceforge.net for library home page. ---------------------
+# See https://github.com/nmusatti/nxpy_ccase. --------------------------------
 
 r"""
 Tools and utilities related to the ClearCase software configuration management product.
